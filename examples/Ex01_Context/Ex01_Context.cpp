fb4cc1ca6d74e33456f259d312d065de639e1dd9
padocity <tyem9721@gmail.com>
2026-03-24 06:44:22 +0900

    Ex01_Context

--- examples/Ex01_Context/Ex01_Context.cpp
+++ examples/Ex01_Context/Ex01_Context.cpp
@@ -4,10 +4,17 @@ using namespace hlab;
 
 int main()
 {
-    vector<const char*> requiredInstanceExtensions = {};
-    bool useSwapchain = false;
+    vector<const char*> requiredInstanceExtensions = {}; // empty
+    bool useSwapchain = false; // off
 
-    Context ctx(requiredInstanceExtensions, useSwapchain);
+    // 출력 없이 초기화만 하는 예제
+    Context ctx(requiredInstanceExtensions, useSwapchain); 
 
     return 0;
 }
+
+/*
+~Context: GPU Device에 관한 정보
+~useSwapchain: 그래픽스 출력이 필요없는 상황에서는 off 하기도 함 (예: Only Compute)
+~requiredInstanceExtensions: OS에 따라 요구하는 Extension 목록
+*/
